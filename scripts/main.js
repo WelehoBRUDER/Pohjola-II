82d6ecd7b2bab125c402146ba8df42b33cdd1a1d
WelehoBRUDER <61124548+WelehoBRUDER@users.noreply.github.com>
2020-11-10 14:41:19 +0200

    sfasf

--- scripts/main.js
+++ scripts/main.js
@@ -1076,7 +1076,7 @@ function battleEnd(condition) {
     let dropped = [];
     if (enemy.drops) {
       for (let drop of enemy.drops) {
-        if(drop == undefined) continue;
+        if(drop == undefined || drop.item == undefined) continue;
         if (drop.chance >= Math.random()) {
           amount = Random(drop.max, drop.min);
           dropped.push({...drop.item, amount: amount});
